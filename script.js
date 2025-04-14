2a267d63bc13eed0031d32fc907d95ff30ea2606
Gavin Hudman <hudman.gavin@gmail.com>
2025-04-14 16:25:07 -0400

    Added flex wrap and width to container in stylesheet to match width of new divs from script. Changed height and width of new divs on script to 50px

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ const container = document.querySelector("#container");
 for (let i = 0; i <256 ; i++) {
     const div = document.createElement("div");
     div.className = "div";
-    div.style.height = "100px";
-    div.style.width = "100px";
+    div.style.height = "50px";
+    div.style.width = "50px";
     div.style.backgroundColor = "black";
     container.appendChild(div);
 }
