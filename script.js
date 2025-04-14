3368f4f1e8bf890008238d4b0ed90d7211924e96
Gavin Hudman <hudman.gavin@gmail.com>
2025-04-14 15:46:32 -0400

    defined container and creation of divs along with div properties

---
+++ script.js
@@ -0,0 +1,6 @@
+const container = document.querySelector(".container");
+const div = document.createElement("div");
+div.className = "div";
+div.style.height = "100px";
+div.style.width = "100px";
+div.style.backgroundColor = "black";
