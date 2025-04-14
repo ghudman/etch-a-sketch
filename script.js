35485a5a1d93ade62e394e74e3635ae8732087f8
Gavin Hudman <hudman.gavin@gmail.com>
2025-04-14 17:35:44 -0400

    Added RGB effect to the mouseover as well as fixed the grid size to be exact each time. background black now too.

--- script.js
+++ script.js
@@ -1,20 +1,44 @@
 const container = document.querySelector("#container");
+const gridButton = document.querySelector("#newGrid");
 
-for (let i = 0; i <256 ; i++) {
-    const div = document.createElement("div");
-    div.className = "div";
-    div.style.height = "30px";
-    div.style.width = "30px";
-    div.style.backgroundColor = "black";
-    container.appendChild(div);
-}
+function getRandomRGB() {
+    const r = Math.floor(Math.random() * 256); // 0–255
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
+    return `rgb(${r}, ${g}, ${b})`;
+  }
 
-const divs = document.querySelectorAll(".div");
+function createDiv(percent) {
+  const div = document.createElement("div");
+  div.className = "div";
+  div.style.flex = `0 0 ${percent}%`;
+  div.style.aspectRatio = '1';
+  div.style.backgroundColor = "white";
 
-divs.forEach(div => {
-  div.addEventListener('mouseover', function() {
-    div.style.backgroundColor = 'gray';
-    div.style.color = 'black';
+  div.addEventListener('mouseover', function () {
+    div.style.backgroundColor = getRandomRGB();
     div.style.cursor = 'pointer';
   });
+
+  container.appendChild(div);
+}
+
+for (let i = 0; i < 256; i++) {
+  createDiv(100 / 16);
+}
+
+gridButton.addEventListener('click', function () {
+  const newSize = parseInt(prompt("Enter new grid size (e.g., 16 for 16x16. Max = 100):"));
+
+  if (newSize > 0 && newSize <= 100) {
+    container.innerHTML = "";
+
+    const percent = 100 / newSize;
+
+    for (let i = 0; i < newSize * newSize; i++) {
+      createDiv(percent);
+    }
+  } else {
+    alert("Please enter a number between 1 and 100.");
+  }
 });
