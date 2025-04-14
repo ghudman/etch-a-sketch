6aa68b6f55d8169a2f6a116d0ff2ceac617ec0f7
Gavin Hudman <hudman.gavin@gmail.com>
2025-04-14 15:59:38 -0400

    Created for loop making 256 divs

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
-const container = document.querySelector(".container");
-const div = document.createElement("div");
-div.className = "div";
-div.style.height = "100px";
-div.style.width = "100px";
-div.style.backgroundColor = "black";
+const container = document.querySelector("#container");
+
+for (let i = 0; i <256 ; i++) {
+    const div = document.createElement("div");
+    div.className = "div";
+    div.style.height = "100px";
+    div.style.width = "100px";
+    div.style.backgroundColor = "black";
+    container.appendChild(div);
+}
