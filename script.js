ad54c6b3dc7f3f11f34ed348799dd24a97621f57
Gavin Hudman <hudman.gavin@gmail.com>
2025-04-14 16:46:02 -0400

    Made div grid smaller. Added button. changed color.

--- script.js
+++ script.js
@@ -3,8 +3,18 @@ const container = document.querySelector("#container");
 for (let i = 0; i <256 ; i++) {
     const div = document.createElement("div");
     div.className = "div";
-    div.style.height = "50px";
-    div.style.width = "50px";
+    div.style.height = "30px";
+    div.style.width = "30px";
     div.style.backgroundColor = "black";
     container.appendChild(div);
 }
+
+const divs = document.querySelectorAll(".div");
+
+divs.forEach(div => {
+  div.addEventListener('mouseover', function() {
+    div.style.backgroundColor = 'gray';
+    div.style.color = 'black';
+    div.style.cursor = 'pointer';
+  });
+});
